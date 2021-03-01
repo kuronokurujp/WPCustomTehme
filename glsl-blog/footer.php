d0f281e2c820cb37b551580ca881ff00ecf3db87
y-uchia <kuronokurujp@gmail.com>
2021-03-01 15:17:10 +0900

    [bugfix]URIのパスに空白が付いていた

--- glsl-blog/footer.php
+++ glsl-blog/footer.php
@@ -20,13 +20,13 @@
 </div><!-- #page -->
 
   <!-- Bootstrap core JavaScript -->
-  <script src="<?php echo get_template_directory_uri(); ?> /vendor/jquery/jquery.min.js"></script>
-  <script src="<?php echo get_template_directory_uri(); ?> /vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- #weggl controller javascript -->
   <?php // WegGL制御コード ?>
-  <script src="<?php echo get_template_directory_uri() ?> /js/libs/common.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?> /js/app_model.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/libs/common.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/app_model.js"></script>
   <script>
     var g_app_model = new AppModel("<?php echo get_template_directory_uri()?>");
     <?php foreach (CustomFieldDairyThumbnail::get_canvas_name_list() as $name): ?>
@@ -34,7 +34,7 @@
     <?php endforeach; ?>
 
   </script>
-  <script src="<?php echo get_template_directory_uri() ?> /js/app.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/app.js"></script>
   <!-- weggl controller javascript# -->
 
 <?php wp_footer(); ?>
